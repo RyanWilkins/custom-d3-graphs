c03dd10adcdc270a73688291abcafb23ee47ab06
Ryan Wilkins <ryanwilkinsuw@gmail.com>
2020-11-15 14:55:41 -0800

    create base loader script

---
+++ js/d3custom.js
@@ -0,0 +1,5 @@
+// Standardized colors for consistency in graphs
+import {p_bg, p_cat, p_seq} from './src/palettes.js';
+import {linearGradient} from './src/palettes.js';
+import {d3barchart} from './src/barchart.js';
+import {d3linechart} from './src/linechart.js';
